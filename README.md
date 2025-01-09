a9b321c3c87bf72ec9e2d2c1263c6affbb69c78f
Morad larbi messaoudi <62658473+moradlarbi@users.noreply.github.com>
2025-01-09 13:01:21 +0000

    setup asynch cnx with rabbitmq

--- README.md
+++ README.md
@@ -5,7 +5,14 @@ password: a4dps9zul7ar1t85
 JAWSDB_URL=mysql://e3v5vqvmprsuzjfi:a4dps9zul7ar1t85@onnjomlc4vqc55fw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/pettlxfldr9yfyx0
 
 compte test: admin@gmail.com 12345678
+prof13@gmail.com 12345678
 
-Prendre en compte active field + mkch delete m bdd f class, salle, user
-Prof : recupere, creer, update avec ses modules 
-get event by class on week, month ectt
+//rabbitmq
+docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:management
+
+python libraries: pip install pymysql pika pulp sqlalchemy pandas datetime
+
+yarn run dev
+python3 test_rabbitmq.py
+
+curl -X POST https://humble-journey-q4rjxp6p9rwfj79-3000.app.github.dev/api/salle -H "Content-Type: application/json" -d '{"name":"Room A","capacity":30,"active":true}'
--- models/salleModels.js
+++ models/salleModels.js
@@ -1,7 +1,7 @@
 import db from "../db.js";
 import amqp from "amqplib";
 // Fetch all salles
-export const getAllSalles = (req, res) => {
+export const getAllSalles = async (req, res) => {
   const query = "SELECT * FROM classRoom";
   db.query(query, (err, results) => {
     if (err) {
@@ -10,6 +10,7 @@ export const getAllSalles = (req, res) => {
         .status(500)
         .json({ status: 500, message: "Internal Server Error" });
     }
+    
     res.status(200).json({ status: 200, message: "OK", data: results });
   });
 };
@@ -31,21 +32,21 @@ export const getSalleById = (req, res) => {
     res.status(200).json({ status: 200, message: "OK", data: results[0] });
   });
 };
-const queue = 'hello_queue';
+const queue = 'update_queue';
 
 async function sendMessage() {
     try {
-        const connection = await amqp.connect('https://ominous-system-j76vgq9q4qghq7pq-5672.app.github.dev:5672');
+        const connection = await amqp.connect('amqp://localhost');
         const channel = await connection.createChannel();
         await channel.assertQueue(queue, { durable: false });
 
-        const message = 'Hello, World!';
+        const message = 'Update Event Table!';
         channel.sendToQueue(queue, Buffer.from(message));
         console.log(`Message sent: ${message}`);
 
         setTimeout(() => {
             connection.close();
-        }, 500);
+        }, 5000);
     } catch (error) {
         console.error('Error sending message:', error);
     }
@@ -62,12 +63,17 @@ export const createSalle = (req, res) => {
   }
 
   const query = "INSERT INTO classRoom (name, capacity,active) VALUES (?, ?, ?)";
-  db.query(query, [name, capacity,active], (err, results) => {
+  db.query(query, [name, capacity,active], async (err, results) => {
     if (err) {
       console.error("Error creating salle:", err);
       return res
         .status(500)
         .json({ status: 500, message: "Internal Server Error" });
+    }
+    try {
+      sendMessage();
+    } catch (error) {
+        console.error('Error sending RabbitMQ message:', error);
     }
     res.status(201).json({
       status: 201,
